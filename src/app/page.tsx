5f787a82e1eba34c9363f9566aec7d3c52f23578
kayp514 <pa.ka12@outlook.com>
2024-12-03 20:37:35 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client'
 
+import { useAuth } from "@tern-secure/nextjs";
 
 export default function Home() {
+  const { userId, isSignedIn } = useAuth();
+
+  if(!isSignedIn){
+    return <h1>Not signed in</h1>
+  }
+  console.log('userId', userId);
   return (
     <div>
       <h1>Home</h1>
