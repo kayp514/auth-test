b6c489cf8f7c7f99ed3f760a57db10bdf4679fa8
kayp514 <pa.ka12@outlook.com>
2024-12-19 14:52:30 -0500

    change 34.3

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/__/auth/:path*',
-        destination: 'https://dev-coffeeconnect-v1.firebaseapp.com/',
+        destination: 'https://dev-coffeeconnect-v1.firebaseapp.com/__/auth/:path*',
       },
       {
         source: '/__/firebase/:path*',
-        destination: 'https://dev-coffeeconnect-v1.firebaseapp.com/',
+        destination: 'https://dev-coffeeconnect-v1.firebaseapp.com/__/firebase/:path*',
       },
     ]
   },
